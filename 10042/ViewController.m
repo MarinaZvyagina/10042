58ab90ce2af962d0b7c93af7bfa38ce87d79d7c3
Марина Звягина <zvyagina.marina@gmail.com>
2017-04-15 12:19:13 +0300

    1

--- 10042/ViewController.m
+++ 10042/ViewController.m
@@ -8,21 +8,96 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) UITableView * tableView;
+@property (nonatomic, copy) NSArray *animals;
+@property (nonatomic, copy) NSTimer *timer;
+
 
 @end
 
 @implementation ViewController
 
+- (instancetype)init {
+    
+    self = [super init];
+    if (self) {
+        self.navigationItem.title = @"Table";
+    }
+    
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = UIColor.whiteColor;
+    
+    //UITableView * tableView = [UITableView new];
+    [self createModel];
+    self.tableView = [UITableView new];
+    
+    self.tableView.frame = CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height);
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    
+    self.tableView.tableFooterView = [UIView new];
+    
+    //self.tableView = [UITableView new];
+    
+    
+    
+    [self.view addSubview:self.tableView];
+ 
+    _timer = [NSTimer scheduledTimerWithTimeInterval:5 target:[NSBlockOperation blockOperationWithBlock:^{
+        self.animals = [self getMeat];
+        [self.timer invalidate];
+        [self.tableView reloadData];
+    }]
+        selector:@selector(main)
+        userInfo:nil
+        repeats:NO  ];
+    
+}
+- (NSArray *) getMeat {
+    return @ [ @"Cat",
+               @"Dog",
+             ];
+}
+
+- (void)createModel {
+    self.animals = @ [ @"Cat",
+                       @"Dog",
+                       @"Pig",
+                       @"Fish",
+                       @"Bird"
+                      ];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.animals.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+    cell.textLabel.text = self.animals[indexPath.row];//[NSString stringWithFormat:@"%ld", indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    UIViewController *vc = [UIViewController new];
+    
+    vc.view.backgroundColor = UIColor.redColor;
+    vc.navigationItem.title = [NSString stringWithFormat:@"%ld", indexPath.row];
+    [self.navigationController pushViewController:vc animated:YES];
 }
 
 
